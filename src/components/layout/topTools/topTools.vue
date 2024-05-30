93b49e0ea8d5bc457929d795f68b88819ae34dad
gogechang <gogechang@163.com>
2024-05-30 22:00:15 +0800

    新增顶部工具栏导入导出

--- src/components/layout/topTools/topTools.vue
+++ src/components/layout/topTools/topTools.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="top-container">
     <ul class="tools-list">
-      <li v-for="item in toolsNav" class="tools-list-item">
-        <img :src="item.icon" alt="" />
+      <li
+        v-for="item in toolsNav"
+        class="tools-list-item"
+        @click="func[item.key]()">
+        <img :src="getIconImg(item.icon)" alt="" />
         <label for="img">{{ item.label }}</label>
       </li>
     </ul>
@@ -10,20 +13,38 @@
 </template>
 
 <script lang="ts" setup>
+import { getIconImg } from "@/utils/globalMethods";
+import { openFileSelect } from "@/utils/topTools/tolTools";
+
 const toolsNav = [
   {
-    icon: "",
+    icon: "save.png",
     label: "保存",
+    key: "save",
   },
   {
-    icon: "",
-    label: "导出",
+    icon: "modelImport.png",
+    label: "导入",
+    key: "import",
   },
   {
-    icon: "",
+    icon: "modelExport.png",
     label: "导出",
+    key: "export",
   },
 ];
+
+let emits = defineEmits(["click"]);
+
+const func: { [key: string]: Function } = {
+  import: async () => {
+    let fileData = await openFileSelect();
+    emits("click", { type: "import", value: fileData });
+  },
+  export: async () =>{
+    emits("click", { type: "export" });
+  }
+};
 </script>
 <style lang="scss" scoped>
 .top-container {
@@ -31,10 +52,33 @@ const toolsNav = [
   background: rgb(29, 29, 29);
   margin-bottom: 8px;
   color: rgb(211, 211, 211);
-  // background-color: ;
   .tools-list {
+    height: 100%;
     display: flex;
-   
+    align-items: center;
+    &-item {
+      font-size: 14px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      width: 60px;
+      img {
+        width: 14px;
+        height: 14px;
+        margin-top: 8px;
+        cursor: pointer;
+      }
+      label {
+        font-size: 12px;
+        margin: 4px 0;
+        cursor: pointer;
+      }
+      &:hover {
+        background-color: black;
+      }
+    }
   }
 }
 </style>

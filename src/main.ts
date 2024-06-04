2178f260669b3f4658a32c6fc5866e4d82e071a7
gogechang <gogechang@163.com>
2024-06-04 18:56:32 +0800

    添加pinia和自动化注册全局组件

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,30 @@ import zhCn from "element-plus/es/locale/lang/zh-cn";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
+import "@/assets/style/resetElement.scss";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(ElementPlus, { locale: zhCn }).use(router);
+app.use(ElementPlus, { locale: zhCn }).use(router).use(pinia);
 app.mount("#app");
+
+const modulesComponent = import.meta.glob(
+  "./components/globalComponents/*/*.vue",
+  {
+    eager: true,
+  }
+);
+
+Object.keys(modulesComponent).forEach((filename) => {
+  // 获取组件配置
+  const componentName = filename
+    .split("/")
+    .pop()
+    .replace(/\.\w+$/, "");
+
+  const component =
+    modulesComponent[filename].default || modulesComponent[filename];
+  app.component(componentName, component);
+});

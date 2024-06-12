978802dc9caea686cb8ed8460f979c4905638bbb
gogechang <gogechang@163.com>
2024-06-12 18:48:48 +0800

    增加禁用功能

--- src/components/tooltipBtn/tooltipBtn.vue
+++ src/components/tooltipBtn/tooltipBtn.vue
@@ -1,16 +1,34 @@
 <template>
-  <el-tooltip  placement="left" v-bind="$attrs">
-    <div class="tooltip-btn" :class="{'tooltip-btn-active': props.active}" @click="sendClick">
-      <img :src="imgUrl">
+  <el-tooltip
+    placement="left"
+    style="position: relative"
+    :effect="'dark'"
+    v-if="disable">
+    <div
+      class="tooltip-btn"
+      :class="{ 'tooltip-btn-active': active, 'tooltip-btn-disable': disable }">
+      <img :src="imgUrl" />
     </div>
+    <template #content>
+      <slot name="content"></slot>
+    </template>
+  </el-tooltip>
+  <el-tooltip placement="left" v-bind="$attrs" :effect="'dark'" v-else>
+    <div class="tooltip-btn" :class="{ 'tooltip-btn-active': active }"  @click="sendClick">
+      <img :src="imgUrl" />
+    </div>
+    <template #content>
+      <slot name="content"></slot>
+    </template>
   </el-tooltip>
 </template>
 
-<script lang='ts' setup>
-import { getIconImg } from '@/utils/globalMethods';
+<script lang="ts" setup>
+import { getIconImg } from "@/utils/globalMethods";
 let props = defineProps<{
   herf: string;
   active: boolean;
+  disable?: boolean;
 }>();
 
 let imgUrl = getIconImg(props.herf);
@@ -20,20 +38,21 @@ function sendClick() {
   emits("click");
 }
 </script>
-<style lang='scss' scoped>
-.tooltip-btn{
+<style lang="scss" scoped>
+.tooltip-btn {
   display: flex;
+  position: relative;
   justify-content: center;
   align-items: center;
   width: 40px;
-  height: 40px;  
+  height: 40px;
   padding: 8px;
   border-radius: 4px;
   background: rgb(52, 51, 55);
   cursor: pointer;
   margin: 8px;
-  transition: all .3s;
-  img {    
+  transition: all 0.3s;
+  img {
     width: 100%;
     height: 100%;
     padding: 3px;
@@ -44,5 +63,20 @@ function sendClick() {
 }
 .tooltip-btn-active {
   background-color: $primaryColor;
+}
+.tooltip-btn-disable {
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: 2;
+    background-color: rgba($color: #000000, $alpha: 0.5);
+  }
+  &:hover {
+    background-color: rgb(52, 51, 55);
+  }
 }
 </style>
